3b16e1509258c34779d44daad3fe57a76bf1b506
Shu Wang <shugewang@hotmail.co.uk>
2023-05-01 18:59:07 +0100

    Add book button

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,7 +36,46 @@ const Header: React.FC = () => {
     </div>
   );
 
-  let right = null;
+  let right = (
+    <div className="right">
+      <Link href="/create">
+        <button>
+          <a>Add book</a>
+        </button>
+      </Link>
+      <style jsx>{`
+        a {
+          text-decoration: none;
+          color: var(--geist-foreground);
+          display: inline-block;
+        }
+
+        p {
+          display: inline-block;
+          font-size: 13px;
+          padding-right: 1rem;
+        }
+
+        a + a {
+          margin-left: 1rem;
+        }
+
+        .right {
+          margin-left: auto;
+        }
+
+        .right a {
+          border: 1px solid var(--geist-foreground);
+          padding: 0.5rem 1rem;
+          border-radius: 3px;
+        }
+
+        button {
+          border: none;
+        }
+      `}</style>
+    </div>
+  );
 
   return (
     <nav>
